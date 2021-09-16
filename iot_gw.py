416cc7b2e7a543fbf3969f2de673bb51b4be5b32
nobuns <gunnar.solheim@semcon.com>
2021-09-16 09:19:51 +0200

    filenames

--- iot_gw.py
+++ iot_gw.py
@@ -1,53 +1,9 @@
-import configparser
-import paho.mqtt.client as mqtt
-import time,ssl
+from mqtt_func import get_client,send_mqtt
+import time
 
-config = configparser.ConfigParser()
-config.read_file(open(r'config.txt'))
+deviceId = "elijah" + str(int(time.time()))
+#client = get_client(deviceId)
+client = get_client()
+node = "EXT"
 
-url = config.get('mqtt','url')
-username = config.get('mqtt','username')
-passwd = config.get('mqtt','passwd')
-port = int(config.get('mqtt','port'))
-device_client = config.get('mqtt','deviceID')
-topic = config.get('mqtt','topic')
-
-#cafile = config.get('mqtt','cafile')
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code: %s" % rc)
-    client.subscribe(topic)
-
-
-def on_publish(client, userdata, mid):
-    print("Message {0} sent from {1}".format(str(mid), deviceId))
-
-def on_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode(),
-        timestamp = 0
-    )
-    print (data)
-    
-def on_log(client, userdata, level, buf):
-    print("log: ",buf)
-    pass
-
-def on_disconnect(client, userdata,rc=0):
-    print ("disconnected: " + str(rc))
-
-client = mqtt.Client(device_client + str(time.time())) #, clean_session=False)
-client.username_pw_set(username,passwd)
-client.on_connect = on_connect
-client.on_message = on_message
-client.on_disconnect = on_disconnect
-client.on_log=on_log
-#client.tls_set("/scripts/mqttgw/ca.crt",tls_version=2)
-#client.tls_set(tls_version=2)
-if port!=1883:
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    client.tls_set_context(context)
-client.connect(url,port)
 client.loop_forever()
-
--- telemetry_mqtt.py
+++ telemetry_mqtt.py
@@ -1,16 +1,23 @@
 from mqtt_func import get_client,send_mqtt
 from metrics import get_metrics
-import time
+import time,socket
 
-hostname = "mailbuns"
+hostname = socket.gethostname()
 deviceId = hostname + str(int(time.time()))
 client = get_client()
 node = "EXT"
+interval = 60
+host_device = "mailbuns"
 
-while 1:
-    mem_usage,cpu_temp,cpu_freq,cpu_usage,mem_total,mem_pct,disk_usage = get_metrics()
-    payload = hostname + "/" + str(mem_usage) + ";" + str(cpu_temp) + ";" + str(cpu_freq) + ";" + str(cpu_usage) + ";"
-    payload+= str(mem_total) + ";" + str(mem_pct) + ";" + str(disk_usage) + "/" + str(time.time())
-    send_mqtt(client,"iot",payload)
-    time.sleep(20)
-client.disconnect()
+try:
+    while True:
+        mem_usage,cpu_temp,cpu_freq,cpu_usage,mem_total,mem_pct,disk_usage = get_metrics()
+        payload = host_device + "/" + str(mem_usage) + ";" + str(cpu_temp) + ";" + str(cpu_freq) + ";" + str(cpu_usage) + ";"
+        payload+= str(mem_total) + ";" + str(mem_pct) + ";" + str(disk_usage) + "/" + str(time.time())
+        send_mqtt(client,"iot",payload)
+        time.sleep(60)
+except KeyboardInterrupt:
+    client.disconnect()
+
+    
+
