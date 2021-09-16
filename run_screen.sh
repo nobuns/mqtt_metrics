766514e01ad6f98d519db1f52060d396e044b9e2
nobuns <gunnar.solheim@semcon.com>
2021-09-16 09:39:39 +0200

    fixed screen script

--- run_screen.sh
+++ run_screen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if ! screen -list | grep -q "pnp_device"; then
-    screen -dmS pnp_device python telemetry_pnp.py
+if ! screen -list | grep -q "mqtt_device"; then
+    screen -dmS mqtt_device python telemetry_mqtt.py
 fi
 
 
